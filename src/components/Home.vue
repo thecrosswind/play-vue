4e4876cf811441f0b3b8d5de24791035bdb5e319
nibaba <863093755@qq.com>
2018-12-26 22:17:51 +0800

    登录页和主页

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,81 @@
 <template>
-  <div class="home">这是home组件</div>
+ <el-container  class="home">
+    <el-header>
+      <div class="logo"></div>
+      <div class="title">
+        <h1>电商管理系统</h1>
+      </div>
+      <div class="logout">
+        <span>欢迎光临</span>
+        <a href="javascript:;" @click="logout">退出</a>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    logout() {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+        localStorage.removeItem('token')
+        this.$router.push('/login')
+      })
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="less" scoped>
+   .home {
+     height: 100%;
+     .el-header {
+     height: 60px;
+     background-color: #B3C1CD;
+     display: flex;
+     .logo {
+      width: 180px;
+      background-image: url('~@/assets/logo.png');
+      background-position: center center;
+      background-size: contain;
+      background-repeat: no-repeat;
+      }
+      .title {
+         flex: 1;
+         h1 {
+         text-align: center;
+         line-height: 60px;
+         color: #fff;
+         font-size: 32px;
+         }
+      }
+      .logout {
+        width: 180px;
+        font-weight: 700;
+        text-align: right;
+        line-height: 60px;
+         a{
+           color: orange;
+         }
+      }
+     }
+     .el-aside {
+    background-color: #545C64;
+     }
+     .el-main {
+     background-color: #D4DFE4;
+     }
+   }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,100 @@
 <template>
-  <div class="login">这个登录组件</div>
+ <div class="login">
+<el-form :model="form" :rules="rules" ref="form" label-width="80px" class="demo-ruleForm">
+   <img src="@/assets/avatar.jpg" alt="">
+  <el-form-item label="用户名" prop="username">
+    <el-input  v-model="form.username" placeholder="请输入用户名"></el-input>
+  </el-form-item>
+    <el-form-item label="密码" prop="password">
+    <el-input  v-model="form.password" placeholder="请输入密码" type="password"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="login">登录</el-button>
+    <el-button @click="reset">重置</el-button>
+  </el-form-item>
+</el-form>
+</div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '用户名不能为空', trigger: 'change' },
+          { min: 3, max: 9, message: '长度在 3 到 9 个字符', trigger: 'change' }
+        ],
+        password: [
+          {required: true, message: '密码不能为空', trigger: 'change'},
+          {min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'change'}
+        ]
+      }
+    }
+  },
+  methods: {
+    reset() {
+      // console.log(this.$refs)
+      this.$refs.form.resetFields()
+    },
+    login() {
+      this.$refs.form.validate(valid => {
+        if (!valid) return false
+        axios({
+          method: 'post',
+          url: 'http://localhost:8888/api/private/v1/login',
+          data: this.form
+        }).then((res) => {
+          console.log(res.data)
+          if (res.data.meta.status === 200) {
+            this.$message({
+              message: '登录成功',
+              type: 'success'
+            })
+            localStorage.setItem('token', res.data.data.token)
+            this.$router.push('/home')
+          } else {
+            this.$message({
+              message: res.data.meta.msg,
+              type: 'error'
+            })
+          }
+        })
+      })
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="less">
+ .login {
+  width: 100%;
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+    .el-form {
+   width:400px;
+   margin: 200px auto;
+   background-color: #fff;
+   padding: 75px 40px 15px;
+   position: relative;
+   border-radius: 10px;
+   img{
+     border-radius:50%;
+     position: absolute;
+     border: 10px solid #fff;
+     left: 50%;
+     top: -70px;
+     transform: translateX(-50%);
+   }
+    .el-button ~ .el-button {
+      margin-left: 80px;
+    }
+  }
+}
 </style>
