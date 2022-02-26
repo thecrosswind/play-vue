d616207ceebdf379672202788b76bf766bbd3f75
shuwei <shuwei@qq.com>
2022-02-26 17:15:07 +0800

    5878

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
- <el-container  class="home">
+  <el-container class="home">
     <el-header>
       <div class="logo"></div>
       <div class="title">
@@ -12,40 +12,41 @@
     </el-header>
     <el-container>
       <el-aside width="200px">
-    <el-menu
-      default-active="1-1"
-      class="el-menu-vertical-demo"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      router>
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>用户管理</span>
-        </template>
-          <el-menu-item index="/users">
+        <el-menu
+          default-active="1-1"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          router
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理cs</span>
+            </template>
+            <el-menu-item index="/users">
               <i class="el-icon-menu"></i>
               <span slot="title">用户列表</span>
             </el-menu-item>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>权限管理</span>
-        </template>
-          <el-menu-item index="/roles">
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="/roles">
               <i class="el-icon-menu"></i>
               <span slot="title">角色列表</span>
             </el-menu-item>
-             <el-menu-item index="/rights">
+            <el-menu-item index="/rights">
               <i class="el-icon-menu"></i>
               <span slot="title">权限列表</span>
             </el-menu-item>
-      </el-submenu>
-    </el-menu>
+          </el-submenu>
+        </el-menu>
       </el-aside>
-      <el-main><router-view/></el-main>
+      <el-main><router-view /></el-main>
     </el-container>
   </el-container>
 </template>
@@ -54,64 +55,64 @@
 export default {
   methods: {
     logout() {
-      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
       }).then(() => {
         this.$message({
-          type: 'success',
-          message: '退出成功!'
-        })
-        localStorage.removeItem('token')
-        this.$router.push('/login')
-      })
-    }
-  }
-}
+          type: "success",
+          message: "退出成功!",
+        });
+        localStorage.removeItem("token");
+        this.$router.push("/login");
+      });
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-   .home {
-     height: 100%;
-     .el-header {
-     height: 60px;
-     background-color: #B3C1CD;
-     display: flex;
-     .logo {
+.home {
+  height: 100%;
+  .el-header {
+    height: 60px;
+    background-color: #b3c1cd;
+    display: flex;
+    .logo {
       width: 180px;
-      background-image: url('~@/assets/logo.png');
+      background-image: url("~@/assets/logo.png");
       background-position: center center;
       background-size: contain;
       background-repeat: no-repeat;
-      }
-      .title {
-         flex: 1;
-         h1 {
-         text-align: center;
-         line-height: 60px;
-         color: #fff;
-         font-size: 32px;
-         }
-      }
-      .logout {
-        width: 180px;
-        font-weight: 700;
-        text-align: right;
+    }
+    .title {
+      flex: 1;
+      h1 {
+        text-align: center;
         line-height: 60px;
-         a{
-           color: orange;
-         }
+        color: #fff;
+        font-size: 32px;
       }
-     }
-     .el-aside {
-    background-color: #545C64;
-      .el-submenu {
+    }
+    .logout {
+      width: 180px;
+      font-weight: 700;
+      text-align: right;
+      line-height: 60px;
+      a {
+        color: orange;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #545c64;
+    .el-submenu {
       width: 200px;
     }
-     }
-     .el-main {
-     background-color: #D4DFE4;
-     }
-   }
+  }
+  .el-main {
+    background-color: #d4dfe4;
+  }
+}
 </style>
